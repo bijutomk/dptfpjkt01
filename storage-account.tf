efc4ceb06011dfb20c053e1acf60d6339528e5d6
Biju Thomas <biju.thomas3@ca.ey.com>
2026-01-05 14:21:57 -0800

    Storage Account addition

--- storage-account.tf
+++ storage-account.tf
@@ -2,9 +2,9 @@ resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier            = var.account_tier
+  account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-  account_kind            = var.account_kind
+  account_kind             = var.account_kind
 
   tags = {
     environment = var.environment
