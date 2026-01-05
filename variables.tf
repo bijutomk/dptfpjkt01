202612be3e13acf4e867db32073895224fa63092
Biju Thomas <biju.thomas3@ca.ey.com>
2026-01-05 14:14:57 -0800

    Storage Account addition

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "subscription_id" {
   type        = string
 }
 
+# RG variables
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -13,3 +14,33 @@ variable "location" {
   type        = string
   default     = "East US" # Optional default value
 }
+
+# Storage Account variables
+variable "storage_account_name" {
+  description = "The name of the storage account (must be globally unique)"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "The tier of the storage account"
+  type        = string
+  default     = "Standard" # Optional default value
+}
+
+variable "account_replication_type" {
+  description = "The replication type of the storage account"
+  type        = string
+  default     = "LRS" # Optional default value
+}
+
+variable "account_kind" {
+  description = "The storage account kind"
+  type        = string
+  default     = "StorageV2" # Optional default value
+}
+variable "environment" {
+  description = "The environment tag for the storage account"
+  type        = string
+  default     = "Development" # Optional default value
+}
+
